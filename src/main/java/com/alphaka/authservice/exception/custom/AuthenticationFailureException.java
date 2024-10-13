8c6fb88640a0477b26c9d972a95cf2abb4a111d4
chanhoim <cksgh201@naver.com>
2024-10-13 20:12:56 +0900

    AK-142 [Feat] 로그인 필터, 로그인 성공, 실패 핸들러 수정

---
+++ src/main/java/com/alphaka/authservice/exception/custom/AuthenticationFailureException.java
@@ -0,0 +1,10 @@
+package com.alphaka.authservice.exception.custom;
+
+import com.alphaka.authservice.exception.ErrorCode;
+
+public class AuthenticationFailureException extends CustomException {
+
+    public AuthenticationFailureException() {
+        super(ErrorCode.AUTHENTICATION_FAILRUE);
+    }
+}
--- src/main/java/com/alphaka/authservice/security/login/handler/CustomLoginFailureHandler.java
+++ src/main/java/com/alphaka/authservice/security/login/handler/CustomLoginFailureHandler.java
@@ -1,23 +1,36 @@
 package com.alphaka.authservice.security.login.handler;
 
-
+import com.alphaka.authservice.exception.custom.AuthenticationFailureException;
+import com.alphaka.authservice.redis.service.LoginAttemptService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class CustomLoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private final LoginAttemptService loginAttemptService;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException, ServletException {
         log.info("자체 로그인 에러: {}", exception.getMessage());
-        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+        // 비밀번호 틀린 경우
+        if (exception instanceof BadCredentialsException) {
+            String email = (String) request.getAttribute("X-Login-Attempt-Email");
+            loginAttemptService.loginFail(email);
+        }
+
+        throw new AuthenticationFailureException();
     }
 }
